1bdce0bfc9bd29493dd9cbfafc5104b5e81e5802
Shiline <shilinemunyambare@gmail.com>
2023-10-12 19:41:54 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * main - prints the numbers 0 to 9 followed by a new line
- *
- * Return: 0 always
+ * print_numbers - print 0-9
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	for (i = 0 ; i <= 9 ; i++)
-		_putcha("%d/n", i);
-	return (0);
+	char c;
+
+	for (c = '0', c <= '9', c++)
+	{
+		_putchar(c);
+	}
+	_putchar('n');
 }
