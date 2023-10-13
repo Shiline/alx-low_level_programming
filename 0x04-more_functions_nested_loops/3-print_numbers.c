82c2376e3cc68d88cc7c153c93dfeb8ac68140ac
Shiline <shilinemunyambare@gmail.com>
2023-10-12 19:12:07 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the numbers 0 to 9 followed by a new line
+ *
+ * Return: 0 always
+ */
+
+void print_numbers(void)
+{
+	for (i = 0 ; i <= 9 ; i++)
+		_putcha("%d/n", i);
+	return (0);
+}
